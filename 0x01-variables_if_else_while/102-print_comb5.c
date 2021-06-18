138c91e67701fbb4805457217b40b79e15578194
Titi999 <57860381+Titi999@users.noreply.github.com>
2021-06-18 15:15:57 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 /**
- * main - program that prints numbers from 0 to 99.
+ * main - program that prints all possible different combinations of 3 digits.
  * Return: 0
  */
-
 int main(void)
 {
 	int c = 0;
+	int f_d;
+	int m_d;
+	int l_d;
 
-	while (c <= 99)
+	while (c <= 999)
 	{
-		putchar(c / 10 + '0');
-		putchar(c % 10 + '0');
-		if (c != 99)
+		f_d = (c / 100 + '0');
+		m_d = (c / 10 % 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if ((f_d < m_d) && (m_d < l_d))
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(f_d);
+			putchar(m_d);
+			putchar(l_d);
+
+			if (c != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		c++;
 	}
